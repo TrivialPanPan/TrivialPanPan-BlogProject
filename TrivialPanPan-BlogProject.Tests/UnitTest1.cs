0ee1c4a2aa48894bd0d79727268b7656c6a5aca9
Florian Chavry <florian.chavry@ai3.fr>
2017-10-09 10:29:22 +0200

    Add project files.

---
+++ TrivialPanPan-BlogProject.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrivialPanPan_BlogProject.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
